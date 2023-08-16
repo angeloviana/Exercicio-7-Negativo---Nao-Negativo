8aca71328491761ae70a27186dfbdfad433d937e
angel <angelo.viana@hotmail.com>
2023-08-15 21:05:16 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício 7 (Negativo - Não Negativo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um múmero inteiro");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 0)
+        {
+            Console.WriteLine("NÃO NEGATIVO");
+        }
+
+        else 
+        {
+            Console.WriteLine("NEGATIVO");
+        }
+    }
+}
